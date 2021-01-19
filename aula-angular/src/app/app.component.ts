6e12e806eea88d9cd2d9477fe017a6891f887867
Alvaro <alvaro.camilloneto@gmail.com>
2021-01-18 21:01:28 -0300

    Início do Angulat

--- aula-angular/src/app/app.component.ts
+++ aula-angular/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+const URL = 'http://api.com';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'aula-angular';
+  title = 'Teste Angular';
+  nome = 'Alvaro';
+
+  numero1 = 5;
+  numero2 = 10;
 }
