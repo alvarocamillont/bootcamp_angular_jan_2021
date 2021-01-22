a5fdbd2b4b910ca956620897f9021f632e15fd42
Alvaro <alvaro.camilloneto@gmail.com>
2021-01-22 19:18:28 -0300

    Rota de clientes

--- front-dwc/src/app/clientes/clientes.module.ts
+++ front-dwc/src/app/clientes/clientes.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { ClientesRoutingModule } from './clientes-routing.module';
 import { ClientesComponent } from './clientes.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [ClientesComponent],
-  imports: [
-    CommonModule,
-    ClientesRoutingModule
-  ]
+  imports: [ClientesRoutingModule, SharedModule],
 })
-export class ClientesModule { }
+export class ClientesModule {}
