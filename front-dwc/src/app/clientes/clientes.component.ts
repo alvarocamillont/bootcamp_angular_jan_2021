fb549b567a4b37533347a269982075e4ac5f9b2b
Alvaro <alvaro.camilloneto@gmail.com>
2021-01-22 19:50:46 -0300

    Tabela de clientes

--- front-dwc/src/app/clientes/clientes.component.ts
+++ front-dwc/src/app/clientes/clientes.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  PoBreadcrumb,
+  PoPageAction,
+  PoTableAction,
+  PoTableColumn,
+} from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
-  styleUrls: ['./clientes.component.css']
+  styleUrls: ['./clientes.component.css'],
 })
 export class ClientesComponent implements OnInit {
+  actions: Array<PoPageAction> = [
+    {
+      label: 'Incluir',
+      url: 'home/clientes/new',
+    },
+  ];
 
-  constructor() { }
+  breadcrumb: PoBreadcrumb = {
+    items: [{ label: 'Home', link: '/home' }, { label: 'Cliente' }],
+  };
 
-  ngOnInit(): void {
-  }
+  items: any;
 
+  colunas: Array<PoTableColumn> = [
+    { property: 'id', label: 'ID', type: 'string' },
+    { property: 'nome', label: 'Nome', type: 'string' },
+    { property: 'endereco', label: 'Endereço', type: 'string' },
+    { property: 'dataNascimento', label: 'Data Nascimento', type: 'date' },
+  ];
+
+  tableActions: Array<PoTableAction> = [
+    { action: this.visualizar.bind(this), label: 'Visualizar' },
+    { action: this.editar.bind(this), label: 'Editar' },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  visualizar() {}
+
+  editar() {}
 }
