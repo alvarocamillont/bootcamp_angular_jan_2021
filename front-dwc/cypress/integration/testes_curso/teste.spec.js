deba987509275513959dad13522de6825430157e
Alvaro <alvaro.camilloneto@gmail.com>
2021-01-29 20:48:44 -0300

    Cypress

--- front-dwc/cypress/integration/testes_curso/teste.spec.js
+++ front-dwc/cypress/integration/testes_curso/teste.spec.js
@@ -7,16 +7,34 @@ context("A aplicação do curso", () => {
 
   // https://on.cypress.io/interacting-with-elements
 
+  function login() {
+    cy.get(
+      ".po-page-login-info-field > po-field-container > .po-field-container > .po-field-container-content > .po-input"
+    ).type("admin");
+
+    cy.get(
+      ".po-page-login-field-size > po-field-container > .po-field-container > .po-field-container-content > .po-input"
+    ).type("admin");
+
+    cy.get(".po-button").click();
+  }
+
   it("deve fazer o login", () => {
+    login();
+    cy.url().should("eq", "http://localhost:4200/home");
+  });
 
-    cy.get(".action-email")
-      .type("fake@email.com {enter}")
-
-    
-    cy.get(".action-disabled")
-      // Ignore error checking prior to type
-      // like whether the input is visible or disabled
-      .type("disabled error checking", { force: true })
-      .should("have.value", "disabled error checking");
+  it("deve incluir um cliente", () => {
+    login();
+    const id = (Math.floor(Math.random() * 10) + 1).toString();
+    cy.visit("http://localhost:4200/home/clientes");
+    cy.get(".po-page-list-actions > po-button > .po-button").click();
+    cy.get(
+      ".po-md-2 > po-field-container > .po-field-container > .po-field-container-content > .po-input"
+    ).type(id);
+    cy.get(
+      ".po-md-10 > po-field-container > .po-field-container > .po-field-container-content > .po-input"
+    ).type("Alvaro");
+    cy.get('[p-icon="po-icon-ok"] > .po-button').click();
   });
 });
